8101a90120b851b9a74eb90fbfab66017c863846
DigitalCoinBRL <sperocoin@gmail.com>
2018-10-21 12:48:29 -0300

    Melhorias de Layout e Otimizações

--- src/qt/statisticspage.cpp
+++ src/qt/statisticspage.cpp
@@ -8,6 +8,7 @@
 #include "bitcoinrpc.h"
 #include <sstream>
 #include <string>
+#include <QDesktopWidget>
 
 using namespace json_spirit;
 
@@ -16,7 +17,15 @@ StatisticsPage::StatisticsPage(QWidget *parent) :
     ui(new Ui::StatisticsPage)
 {
     ui->setupUi(this);
-
+//adding android size code
+            QFont font;
+            font.setFamily(font.defaultFamily());
+            QRect rec = QApplication::desktop()->screenGeometry();
+            int fS=std::max(7,(int)rec.width()/80);
+            font.setPointSize(fS);
+            this->setFont(font);
+            this->setFixedWidth((int)rec.width());
+            this->setFixedHeight((int)(rec.height()*0.8));
     setFixedSize(400, 420);
 
     connect(ui->startButton, SIGNAL(pressed()), this, SLOT(updateStatistics()));
