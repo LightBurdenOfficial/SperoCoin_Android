a8bdfc157f31c81de192538613aa47769e452cef
DigitalCoinBRL <sperocoin@gmail.com>
2018-10-25 11:34:11 -0300

    Atualizações para melhor estabilidade

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -161,7 +161,7 @@ void Intro::pickDataDirectory()
                 boost::filesystem::create_directory(GUIUtil::qstringToBoostPath(dataDir));
                 break;
             } catch (const fs::filesystem_error&) {
-                QMessageBox::critical(0, tr("idealcash-qt"),
+                QMessageBox::critical(0, tr("SperoCoin-qt"),
                     tr("Error: Specified data directory \"%1\" cannot be created.").arg(dataDir));
                 /* fall through, back to choosing screen */
             }
